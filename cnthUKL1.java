ef71c7ed37b67276d495ea6d5e2d62f5f5a1dc2f
elzharrr <thewelut59@gmail.com>
2025-11-04 11:44:19 +0700

    Add files via upload

--- cnthUKL1.java
+++ cnthUKL1.java
@@ -4,31 +4,32 @@ import java.util.Scanner;
 
 public class cnthUKL1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Masukkan Berat Paket (kg): ");
-        double berat = scanner.nextDouble();
+        int hasil = 0;
+        System.out.println("=====Zel Ekspedisi=====");
+        System.out.println("Selamat datang di layanan Ekspedisi kami!");
+        System.out.print("Masukkan berat barang (kg): ");
+        int berat = input.nextInt();
+        System.out.println("Masukan jarak tempuh pengiriman (km): ");
+        int jarak = input.nextInt();
+        System.out.println("Masukkan Panjang Barang (m): ");
+        int panjang = input.nextInt();
+        System.out.println("Masukkan Lebar Barang (m): ");
+        int lebar = input.nextInt();
+        System.out.println("Masukkan Tinggi Barang (m): ");
+        int tinggi = input.nextInt();
 
-        System.out.println("Masukkan Jarak Pengiriman (km): ");
-        double jarak = scanner.nextDouble();
-
-        System.out.println("Masukkan Volume Paket (cm3): ");
-        double volume = scanner.nextDouble();
-
-        double biayaPerKG; 
-        if ( jarak <= 10 ) {
-            biayaPerKG = 4250;
-        } else {
-            biayaPerKG = 6000;
+        if(jarak < 10 || jarak == 10) {
+            hasil += 4250 * berat;
+        }else if(jarak > 10) {
+            hasil += 6000 * berat;
+        }
+        int volume = panjang * lebar * tinggi;
+        if (volume > 100) {
+            hasil += 50000;
         }
 
-        double biayaVolume = 0;
-        if ( volume > 100 ); {
-            biayaVolume = 50000;
-        }
-
-        double totalBiaya = (biayaPerKG * berat) + biayaVolume;
-
-        System.out.println("Total Biaya Pengiriman: Rp " + totalBiaya);
+        System.out.println("Total biaya pengiriman: Rp " + hasil);
     }
 }
